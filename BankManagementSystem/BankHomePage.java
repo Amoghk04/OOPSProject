cd9d844b86a200781c5d652edb7e32463480bd01
Amit Madhbahavi <154544727+A-m-i-t-M@users.noreply.github.com>
2024-02-09 16:28:46 +0530

    Update BankHomePage.java

--- BankManagementSystem/BankHomePage.java
+++ BankManagementSystem/BankHomePage.java
@@ -1,12 +1,12 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.plaf.basic.BasicBorders.ButtonBorder;
 
 public class BankHomePage extends Frame implements ActionListener {
     Button runProgramBtn;
@@ -14,24 +14,21 @@ public class BankHomePage extends Frame implements ActionListener {
 
     public BankHomePage() {
         setLayout(new FlowLayout());
-
         JPanel logoPanel = new JPanel();
         ImageIcon logoIcon = new ImageIcon("C:\\Users\\Amit\\OneDrive\\Desktop\\OOPS\\Ramaiah.png"); 
         JLabel logoLabel = new JLabel(logoIcon);
         logoPanel.add(logoLabel);
         add(logoPanel, BorderLayout.NORTH);
-
         JLabel titleLabel = new JLabel("Welcome to RIT-MiniBank");
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 22));
         add(titleLabel, BorderLayout.CENTER);
-
         JPanel buttonPanel = new JPanel();
-        JButton runProgramBtn = new JButton("Launch RIT-MiniBank");
+        runProgramBtn = new Button("Launch Rit-MiniBank");
         buttonPanel.add(runProgramBtn);
+        buttonPanel.setOpaque(false);buttonPanel.setBackground(Color.RED);
         add(buttonPanel, BorderLayout.SOUTH);
         runProgramBtn.addActionListener(this);
-
         setTitle("Rit-MiniBank");
         setSize(400, 400);
         setLocationRelativeTo(null);
@@ -42,17 +39,15 @@ public class BankHomePage extends Frame implements ActionListener {
             }
         });
     }
-
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == runProgramBtn) {
-            try {
+             try {
                 bank.main(new String[]{});
             } catch (IOException ex) {
                 System.out.println("ERROR: Running the program");
             }
         }
     }
-
     public static void main(String[] args) {
         new BankHomePage();
     }
